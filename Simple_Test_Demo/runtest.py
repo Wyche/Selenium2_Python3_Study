e77c24bbecfd253dd724abdfd2df76f209b3c022
Wyche Wang_HPE <yi-chen.wang@hpe.com>
2017-01-05 16:55:28 +0800

    Complete auto send email

--- Simple_Test_Demo/runtest.py
+++ Simple_Test_Demo/runtest.py
@@ -5,14 +5,14 @@ import unittest, time, smtplib, os
 
 #define sending email ------------------------------------------------------
 def send_mail(file_new):
-    f = open(file_new)
+    f = open(file_new, 'rb')
     mail_body = f.read()
     f.close()
 
     msg = MIMEText(mail_body, 'html', 'uft-8')
     msg['Subject'] = Header("自动化测试报告", 'utf-8')
-    msg['From'] = "wyche_wang@outlook.com"
-    msg['To'] = "yi-chen.wang@hpe.com"
+    msg['From'] = ""
+    msg['To'] = ""
 
     smtp = smtplib.SMTP()
     smtp.connect("smtp3.hpe.com")
@@ -32,19 +32,18 @@ def new_report(testreport):
 if __name__ == '__main__':
 
     test_dir = '.\\test_case'
-    test_report = '.\\test_case'
+    test_report = '.\\report'
+    
     discover = unittest.defaultTestLoader.discover(test_dir,pattern='test_*.py')
 
-report_dir = './report'
-
-if __name__ == '__main__':
-
     now = time.strftime('%Y-%m-%d %H_%M_%S')
-    filename = report_dir + '/' + now + 'result.html'
+    filename = test_report + '\\' + now + 'result.html'
     fp = open(filename, 'wb')
     runner = HTMLTestRunner(stream=fp,
                             title='测试报告',
                             description='用例执行情况: ')
-
     runner.run(discover)
     fp.close()
+
+    new_report = new_report(test_report)
+    send_mail(new_report)
